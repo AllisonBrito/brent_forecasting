99ef034191136f38e5a02f533b9c3ccbc79cff3b
Allison Brito <102196848+AllisonBrito@users.noreply.github.com>
2024-11-18 23:51:24 -0300

    Update and rename main.py to Storytelling and Forecast.py

--- Storytelling and Forecast.py
+++ Storytelling and Forecast.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from storytelling import show_storytelling
-from dashboard import show_dashboard
 from forecast import show_forecasting
 
 def main():
@@ -9,16 +8,13 @@ def main():
 
     # Sidebar para navegação entre as páginas
     st.sidebar.title("Navegação")
-    page = st.sidebar.radio("Ir para", ["Storytelling", "Dashboard", "Forecasting"])
+    page = st.sidebar.radio("Ir para", ["Storytelling", "Forecasting"])
 
     # Renderização das páginas com base na seleção do usuário
     if page == "Storytelling":
         show_storytelling()
-    elif page == "Dashboard":
-        show_dashboard()
     elif page == "Forecasting":
         show_forecasting()
-
 
 if __name__ == "__main__":
     main()
